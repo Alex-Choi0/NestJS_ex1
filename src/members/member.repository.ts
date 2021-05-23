ce068548c0c32f7e705d8b40e49c4f5685142fea
Alex-Choi <choijeaho86@gmail.com>
2021-05-23 09:31:43 +0900

    branch7_deleteMember

--- src/members/member.repository.ts
+++ src/members/member.repository.ts
@@ -1,11 +1,13 @@
 // member.repository.ts : /src/members
 
-
 import { Member } from './member.entity';
 import { EntityRepository, Repository } from 'typeorm';
 
 // 내부에 에러가 발생시 사용.
-import { InternalServerErrorException } from '@nestjs/common';
+import { 
+    BadRequestException, 
+    InternalServerErrorException 
+} from '@nestjs/common';
 
 // Member entity를 불러서 DB의 테이블 생성
 @EntityRepository(Member)
@@ -85,6 +87,68 @@ export class MemberRepository extends Repository<Member> {
 
         // password는 민감한 정보이므로 Service쪽에 보내지 않는다.
         delete member.password;
+
+        // member를 Service쪽으로 return한다.
+        return member
+    }
+
+    // Member DB정보를 수정한다.
+    async deleteMember(
+        memberId : number,
+        password : string,
+        email : string,
+    ) : Promise <Member> {
+
+        // this는 DB를 말한다.
+        // DB에서 해당 id와 일치하는 memberId를 찾는다.
+        const member = await this.findOne({ id : memberId});
+        console.log(member);
+        try{
+
+            // member가 존재하지 않을시
+            if(!member){
+                throw {
+                    statusCode : 400,
+                    message : "DB can't find the record"
+                }
+            }
+
+            // password, email이 생략시
+            else if(
+                !password || 
+                !email
+            ){
+                throw {
+                    statusCode : 400,
+                    message : "There are no password or email"
+                }
+            }
+
+            // member의 입력정보와 DB의 정보를 비교
+            else if(
+                member.password === password &&
+                member.email === email
+            ){
+                await this.delete({id : memberId});
+            }
+
+            
+        }
+        catch(err){
+            // error가 발생하면 해당코드와 에러 message를 service쪽에
+            // 보내준다.
+            console.log(err);
+
+            // 에러코드에 따라 다르게 respone한다.
+            if(err.statusCode === 400){
+                throw new BadRequestException(err.message)
+            }
+
+            // 기타에러는 내부에러로 처리
+            throw new InternalServerErrorException(err.message);
+            // 여기서 코드는 마무리됨
+        }
+
 
         // member를 Service쪽으로 return한다.
         return member
