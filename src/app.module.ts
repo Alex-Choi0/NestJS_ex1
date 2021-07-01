dd9c0da1d0a66e283525e4efa2778435938376e1
choijeaho86@gmail.com <choijeaho86@gmail.com>
2021-07-01 15:59:05 +0900

    add to read parameter

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-
-
+import { CoffeeController } from './modules/coffee/coffee.controller';
 
 
 @Module({
   // ProductsModule를 현 AppModule에 추가하였다.
   imports: [
     ],
-  controllers: [AppController],
+  controllers: [AppController, CoffeeController],
   providers: [AppService],
 })
 export class AppModule {}
